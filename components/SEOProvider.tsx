8ed62718c7d4bab8c91e1c8df4d7dee598deacda
j_abed <16858833+j-abed@users.noreply.github.com>
2025-09-23 21:04:45 -0400

    SEO modernized

--- components/SEOProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { DefaultSeo } from 'next-seo'
-import seoConfig from '@/next-seo.config'
-
-export default function SEOProvider() {
-  return <DefaultSeo {...seoConfig} />
-}
